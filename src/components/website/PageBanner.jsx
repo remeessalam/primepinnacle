d2cca9d94bbdaf401e6b643c81b10c42df730ecb
remeessalam <remeesreme4u@gmail.com>
2025-01-08 18:48:54 +0530

    change text size and weight in service page banner

--- src/components/website/PageBanner.jsx
+++ src/components/website/PageBanner.jsx
@@ -7,12 +7,16 @@ const PageBanner = ({ title, desc }) => {
       className="w-full min-h-[55vh] h-fit py-[1rem] bg-cover bg-center relative flex items-center justify-center"
       style={{ backgroundImage: `url(${bannerImg})` }}
     >
-      <div className="absolute top-0 w-full h-full bg-primary/20"></div>
+      <div className="absolute top-0 w-full h-full bg-black/30"></div>
       {/* <div className="absolute top-1/2 -translate-y-[20%] left-1/2 -translate-x-1/2 w-full"> */}
       <div className="w-full translate-y-[15%]">
         <div data-aos="fade-up" className="wrapper text-center">
           <h1 className="heading-1 text-white">{title}</h1>
-          {desc && <p className="desc mt-3 max-w-[55rem] mx-auto">{desc}</p>}
+          {desc && (
+            <p className="desc mt-3 max-w-[55rem] mx-auto drop-shadow-2xl !text-white !text-base !font-medium">
+              {desc}
+            </p>
+          )}
         </div>
       </div>
     </div>
